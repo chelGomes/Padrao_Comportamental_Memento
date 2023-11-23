8d2073a9bc7bede9160e61cad71532c1985d817c
chelGomes <68570496+chelGomes@users.noreply.github.com>
2023-11-23 16:22:46 -0300

    implementacao dos casos de testes

---
+++ src/test/java/ContaBancariaTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ContaBancariaTest {
+    @Test
+    void deveArmazenarEstados() {
+        ContaBancaria contaBancaria = new ContaBancaria();
+        contaBancaria.setEstado(ContaBancariaEstadoAtiva.getInstance());
+        assertEquals(2, contaBancaria.getEstados().size());
+    }
+
+    @Test
+    void deveRetornarEstadoInicial() {
+        ContaBancaria contaBancaria = new ContaBancaria();
+        contaBancaria.setEstado(ContaBancariaEstadoEncerrada.getInstance());
+        contaBancaria.setEstado(ContaBancariaEstadoTransferida.getInstance());
+        contaBancaria.restauraEstado(0);
+        assertEquals(ContaBancariaEstadoAtiva.getInstance(), contaBancaria.getEstado());
+    }
+
+    @Test
+    void deveRetornarEstadoAnterior() {
+        ContaBancaria contaBancaria = new ContaBancaria();
+        contaBancaria.setEstado(ContaBancariaEstadoAtiva.getInstance());
+        contaBancaria.setEstado(ContaBancariaeEstadoCongelada.getInstance());
+        contaBancaria.setEstado(ContaBancariaEstadoEncerrada.getInstance());
+        contaBancaria.setEstado(ContaBancariaEstadoTransferida.getInstance());
+        contaBancaria.restauraEstado(2);
+        assertEquals(ContaBancariaEstadoAtiva.getInstance(), contaBancaria.getEstado());
+    }
+
+    @Test
+    void deveRetornarExcecaoIndiceInvalido() {
+        try {
+            ContaBancaria contaBancaria = new ContaBancaria();
+            contaBancaria.restauraEstado(0);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Índice inválido", e.getMessage());
+        }
+    }
+}
